683676971779fa430ae45280c5ac8299109d31b2
Nikolay <nikolay.orevkov@gmail.com>
2020-10-27 14:10:46 +0300

    Переписали проект с использованием Blueprints. Добавили меню и декоратор @admin_required

---
+++ webapp/news/views.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, current_app, render_template
+
+from webapp.news.models import News
+from webapp.weather import weather_by_city
+
+blueprint = Blueprint('news', __name__)
+
+@blueprint.route("/")
+def index():
+    page_title = "Новости Python"
+    weather = weather_by_city(current_app.config['WEATHER_DEFAULT_CITY'])
+    news_list = News.query.order_by(News.published.desc()).all()
+    return render_template('news/index.html', city=current_app.config['WEATHER_DEFAULT_CITY'], 
+        weather=weather, page_title=page_title, news_list=news_list)
